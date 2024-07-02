9b9ea91270e3ddd3f3fe70b4cd610ae1e448cd6c
Ghada Ahmed <88281680+ga72@users.noreply.github.com>
2024-07-02 22:50:34 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,24 @@
+var typed = new Typed(".auto-typed" , {
+strings : ["Larry Daniels " , 'Designer ' , 'Developer'] ,
+typeSpeed : 80 ,
+backSpeed : 80,
+looped : true
+});
+
+
+// const text = document.querySelector(".sec")
+
+// const textLoad = ()=>{
+//     setTimeout(()=>{
+//         text.textContent = "Larry Daniels";
+//     } , 0);
+//     setTimeout(()=>{
+//         text.textContent = "Designer ";
+//     } , 4000);
+//     setTimeout(()=>{
+//         text.textContent = "Developer";
+//     } , 8000);
+
+// }
+
+// textLoad();
